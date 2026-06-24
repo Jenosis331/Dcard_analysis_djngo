2f13b7ce0361230120cf5060f1174711bd2d9b98
Jenosis331 <153162082+Jenosis331@users.noreply.github.com>
2026-06-24 19:48:12 +0800

    Delete -- SQLite.sql

--- -- SQLite.sql
+++
@@ -1,9 +0,0 @@
--- SQLite
-DELETE FROM dcard_data1_all
-WHERE category = 'D';
-DELETE FROM dcard_data2_preprocessed_all
-WHERE category = 'D';
-DELETE FROM dcard_data3_tokenpos_all
-WHERE category = 'D';
-DELETE FROM dcard_top_person
-WHERE category = 'D';
